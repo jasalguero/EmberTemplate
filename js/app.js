cf448013d5c6bb2489ca3fba1db75617ebaeb1fb
jasalguero <jose.salguero@groupon.com>
2013-01-17 15:31:45 +0100

    Created sections in the app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
 var App = Ember.Application.create();
 
+
+/*******************************/
+/*      MODEL                  */
+/*******************************/
+
+
+/*******************************/
+/*      CONTROLLERS            */
+/*******************************/
 App.ApplicationController = Ember.Controller.extend();
+
+
+/*******************************/
+/*      VIEWS                  */
+/*******************************/
 App.ApplicationView = Ember.View.extend({
-  templateName: 'application'
+    templateName: 'application'
 });
 
+/*******************************/
+/*      UTILS                  */
+/*******************************/
 App.Router = Ember.Router.extend({
-  root: Ember.Route.extend({
-    index: Ember.Route.extend({
-      route: '/'
+    root: Ember.Route.extend({
+        index: Ember.Route.extend({
+            route: '/'
+        })
     })
-  })
 })
 
+/*******************************/
+/*      UTILS                  */
+/*******************************/
 App.initialize();
